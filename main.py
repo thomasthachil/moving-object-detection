033cc15939df0117d61ca7b36c07777e76c43a3a
Thomas Thachil <github@thomasthachil.com>
2017-04-26 01:10:26 -0400

    new method and videos

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ import numpy as np
 import cv2
 
 # capture frames from a video
-cap = cv2.VideoCapture('videos/video.mp4')
+# bgsMOG = cv2.BackgroundSubtractorMOG()
+cap = cv2.VideoCapture('videos/intersectionSmall.mp4')
 
 # Trained XML classifiers describes some features of some object we want to detect
-car_cascade = cv2.CascadeClassifier('data/cars.xml')
+car_cascade = cv2.CascadeClassifier('data/cars3.xml')
+# hog = cv2.HOGDescriptor("data/vehicle_detector.yml")
 ret, frame = cap.read()
 
 # loop runs if capturing has been initialized.
 while(cap.isOpened()):
     # reads frames from a video
     ret, frame = cap.read()
-
+    # cv2.imshow('Vehicle Detection Original', frame)
     # convert to gray scale of each frames
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-
+    # fgmask = bgsMOG.apply(frame)
+    # cv2.imshow('Vehicle Detection', fgmask)
     # Detects cars of different sizes in the input image
-    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+    cars = car_cascade.detectMultiScale(frame, 1.9, 1)
 
     # To draw a rectangle in each cars
     for (x,y,w,h) in cars:
